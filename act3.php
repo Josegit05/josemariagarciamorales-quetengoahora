0ed6133d68230de5ade5accaff3f739756475261
Josegit05 <113434746+Josegit05@users.noreply.github.com>
2022-09-19 10:49:32 +0100

    Update act3.php

--- act3.php
+++ act3.php
@@ -1,27 +1,31 @@
 <?php
 
 $asignaturas =[
-   [
-      ["EMR" =>"MARIA DEL SOL GARCIA TARAJANO","Aula 201"],
-      ["DSW" =>"SERGIO RAMOS SUAREZ","Aula 201"],
-      ["DEW" =>"MARIA DEL CARMEN RODRIGUEZ SUAREZ","Aula 201"],
-      ["DPL" =>"MARIA ANTONIA MONTESDEOCA VIERA","Aula 201"],
-      ["DOR" =>"Diseño de interfaces web","Aula 201"],
-      ["DSW" =>"SERGIO RAMOS SUAREZ","Aula 201"],
-   ]    
+   
+      "EMR" =>"MARIA DEL SOL GARCIA TARAJANO",
+      "DSW" =>"SERGIO RAMOS SUAREZ",
+      "DEW" =>"MARIA DEL CARMEN RODRIGUEZ SUAREZ",
+      "DPL" =>"MARIA ANTONIA MONTESDEOCA VIERA",
+      "DOR" =>"ERMIS PAPAKONSTANTINOU BAEZ",
+      "DSW" =>"SERGIO RAMOS SUAREZ",
+   
 ];
 
 $horario =[
-  [
-    ["Lunes" =>"8","9","10","11","12","13"],
-    ["Martes" =>"8","9","10","11","12","13"],
-    ["Miércoles" =>"8","9","10","11","12","13"],
-    ["Jueves" =>"8","9","10","11","12","13"],
-    ["Viernes" =>"8","9","10","11","12","13"],
-  ]
+  
+    "Lunes" =>"8:00","8:55","9:50","11:15","12","13",
+    "Martes" =>"8","9","10","11","12","13",
+    "Miércoles" =>"8","9","10","11","12","13",
+    "Jueves" =>"8","9","10","11","12","13",
+    "Viernes" =>"8","9","10","11","12","13",
+  
 ];
 
+
 foreach($asignaturas as $asignatura => $valor){
-    print_r('<br>El correo de '.$asignatura.' es '.$valor) ;
+  
+  foreach($horario as $horarios => $valor2){
+  print_r('<br>El '.$horarios. ' tenemos a las '.$valor2.' de '.$asignatura.' en el aula 201.') ;
+  }
 }
-?>,
+?>
